62cb6d90c970a36cdaab0532fe8cd166b0fd5fba
rémi <remirobert33530@gmail.com>
2015-02-23 23:45:18 +0100

    add file manager

---
+++ Wizzem/Wizzem/Utils/FileManager.h
@@ -0,0 +1,17 @@
+//
+//  FileManager.h
+//  Wizzem
+//
+//  Created by Remi Robert on 23/02/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileManager : NSObject
+
++ (BOOL) isFileExist:(NSString *)file;
++ (BOOL) deleteFile:(NSString *)file;
++ (BOOL) writeFile:(NSString *)file dataFile:(NSData *)data;
+
+@end
---
+++ Wizzem/Wizzem/Utils/FileManager.m
@@ -0,0 +1,45 @@
+//
+//  FileManager.m
+//  Wizzem
+//
+//  Created by Remi Robert on 23/02/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+#import "FileManager.h"
+
+@implementation FileManager
+
++ (BOOL) isFileExist:(NSString *)file {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:file];
+    
+    return [fileManager fileExistsAtPath:filePath];
+}
+
++ (BOOL) deleteFile:(NSString *)file {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:file];
+    NSError *error = nil;
+    
+    if (![fileManager removeItemAtPath:filePath error:&error]) {
+        NSLog(@"[Error] %@ (%@)", error, filePath);
+        return false;
+    }
+    return true;
+}
+
++ (BOOL) writeFile:(NSString *)file dataFile:(NSData *)data {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:file];
+    
+    if ([fileManager isWritableFileAtPath:filePath]) {
+        return [data writeToFile:filePath atomically:true];
+    }
+    return false;
+}
+
+@end
