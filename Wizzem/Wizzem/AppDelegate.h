8c39f96e9660e305a9028de0a4e2e2019946bdea
rémi <remirobert33530@gmail.com>
2015-02-23 18:22:06 +0100

    add base project

---
+++ Wizzem/Wizzem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wizzem
+//
+//  Created by Remi Robert on 23/02/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wizzem/Wizzem/PhotoViewController.m
@@ -0,0 +1,25 @@
+//
+//  PhotoViewController.m
+//  Wizzem
+//
+//  Created by Remi Robert on 23/02/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+#import "PhotoViewController.h"
+
+@interface PhotoViewController ()
+
+@end
+
+@implementation PhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
