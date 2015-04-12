37a29a82544ea1511ae27ecbcedb0086530960ae
rémi  <remirobert33530@gmail.com>
2015-04-12 13:53:10 +0200

    add unique observer for notification center

--- Wizzem/Wizzem/Controllers/TabBarViewController.m
+++ Wizzem/Wizzem/Controllers/TabBarViewController.m
@@ -32,12 +32,19 @@
     }
 }
 
+- (void)addUniqueObserver:(id)observer selector:(SEL)selector name:(NSString *)name object:(id)object {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:observer name:name object:object];
+    [[NSNotificationCenter defaultCenter] addObserver:observer selector:selector name:name object:object];
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.tabBar.tintColor = [UIColor redColor];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentController:) name:PRESENT_CONTROLLER_NOTIFICATION object:nil];
+
+    [self addUniqueObserver:self selector:@selector(presentController:) name:PRESENT_CONTROLLER_NOTIFICATION object:nil];
     
     UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
     button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
