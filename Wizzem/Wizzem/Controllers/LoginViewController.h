7966197a931b7d64192a02e40d873eeb062dcbfa
rémi  <remirobert33530@gmail.com>
2015-04-26 11:19:09 +0200

    update tabbarController form login signup. add view controller

---
+++ Wizzem/Wizzem/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Wizzem
+//
+//  Created by Remi Robert on 26/04/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
